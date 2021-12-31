6caa594d4582720c429fd6303b0de6a22568bddb
gohjoseph <josephgoh7@gmail.com>
2021-12-31 20:17:46 +0800

    Add skeleton

--- WaterWorld/Class1.cs
+++ WaterWorld/Class1.cs
@@ -2,11 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
 
 namespace WaterWorld
 {
-    public class Class1
+    [ApiVersion(2, 1)]
+    public class WaterWorldPlugin : TerrariaPlugin
     {
+        /// <summary>
+        /// Gets the author(s) of this plugin
+        /// </summary>
+        public override string Author => "PotatoCider, Tuck";
+
+        /// <summary>
+        /// Gets the description of this plugin.
+        /// A short, one lined description that tells people what your plugin does.
+        /// </summary>
+        public override string Description => "Creates a water world.";
+
+        /// <summary>
+        /// Gets the name of this plugin.
+        /// </summary>
+        public override string Name => "Water World";
+
+        /// <summary>
+        /// Gets the version of this plugin.
+        /// </summary>
+        public override Version Version => new Version(1, 0, 0);
+
+        /// <summary>
+        /// Initializes a new instance of the TestPlugin class.
+        /// This is where you set the plugin's order and perfrom other constructor logic
+        /// </summary>
+        public WaterWorldPlugin(Main game) : base(game)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles plugin initialization. 
+        /// Fired when the server is started and the plugin is being loaded.
+        /// You may register hooks, perform loading procedures etc here.
+        /// </summary>
+        public override void Initialize()
+        {
+
+        }
+
+        /// <summary>
+        /// Handles plugin disposal logic.
+        /// *Supposed* to fire when the server shuts down.
+        /// You should deregister hooks and free all resources here.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Deregister hooks here
+            }
+            base.Dispose(disposing);
+        }
     }
 }
